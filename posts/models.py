a1427aa3fa937b2ab0e8cd9b657c7189ca0226fa
uhuieee <ivlad9454@gmail.com>
2026-05-29 13:53:49 +0600

    update: deleted venv

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField()
-    content = models.TextField
-    rate = models.IntegerField
+    content = models.TextField()
+    rate = models.IntegerField()
 
 # Create your models here.
